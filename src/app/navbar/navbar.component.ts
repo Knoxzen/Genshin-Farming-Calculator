118e594499bba4b3de77eaf2a40e1f0e51b205b0
kundanpradhan1402 <kundanpradhan1402@gmail.com>
2024-03-31 17:48:31 +0000

    NAV Buttons Working

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {Clipboard} from '@angular/cdk/clipboard';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  constructor(private clipboard: Clipboard) {}
+  clickFunction(){
+        this.clipboard.copy(window.location.href);
+        alert('Link copied to clickboard')
+  }
 }
