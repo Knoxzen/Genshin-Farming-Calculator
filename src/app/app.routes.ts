cdd458003512fa823c61ddf134361308d94aa962
kundanpradhan1402 <kundanpradhan1402@gmail.com>
2024-03-31 17:48:46 +0000

    Optimize

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { TalentLevelUp3Component } from './talent-level-up-3/talent-level-up-3.component';
 import { CharLevelUp3Component } from './char-level-up-3/char-level-up-3.component';
 import { WeaponStone4Component } from './weapon-stone-4/weapon-stone-4.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/charLvlUp', pathMatch: 'full' },
+  { path: '', component: AppComponent },
   { path: 'charLvlUp', component: CharLevelUp3Component },
   { path: 'weaponStone', component: WeaponStone4Component },
-  { path: 'talentLvlUp', component: TalentLevelUp3Component },
+  { path: 'talentLvlUp', component: TalentLevelUp3Component }
 ];
