eab1fbb3906fb3bf259f017e90ec5d7f315f650e
kundanpradhan1402 <kundanpradhan1402@gmail.com>
2024-03-31 06:26:54 +0000

    Design Adjust

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,17 @@ import { ModuleSelectorComponent } from "./mod-selector/mod-selector.component";
 import { CharLevelUp3Component } from './char-level-up-3/char-level-up-3.component';
 import { WeaponStone4Component } from './weapon-stone-4/weapon-stone-4.component';
 import { TalentLevelUp3Component } from './talent-level-up-3/talent-level-up-3.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InfoComponent } from './info/info.component';
 
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, CharLevelUp3Component,TalentLevelUp3Component, WeaponStone4Component, ModuleSelectorComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, CharLevelUp3Component, TalentLevelUp3Component,
+    WeaponStone4Component, ModuleSelectorComponent, NavbarComponent, InfoComponent]
 })
 export class AppComponent {
   title = 'genshin-calculator';
